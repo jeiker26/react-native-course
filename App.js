2c043ef62b4800cddbd87b5713107020a7e353f5
jeiker26 <jesusgraficap@gmail.com>
2023-10-09 22:02:20 +0200

    google maps done

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import 'react-native-gesture-handler';
 import {RecoilRoot} from 'recoil';
 import {NavigationContainer} from '@react-navigation/native';
 import TabsIndexScreen from './components/screens/TabsIndexScreen';
+import {enableLatestRenderer} from 'react-native-maps';
 
+enableLatestRenderer();
 export default function App() {
   return (
     <RecoilRoot>
--- components/screens/tabs/home/HomeScreen.jsx
+++ components/screens/tabs/home/HomeScreen.jsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+
+export const MARKERS_DATA = [
+  {
+    id: '1',
+    latitude: 37.78825,
+    longitude: -122.4324,
+    color: '#2F3136',
+  },
+];
+
 const HomeScreen = ({navigation}) => {
+  function requestLocationPermission() {
+    Geolocation.requestAuthorization(
+      () => console.log('Permiso concedido'),
+      () => console.error('Permiso de ubicación denegado'),
+    );
+  }
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bienvenido a la "Tienda"</Text>
-      <Button
-        title="Ver tienda"
-        onPress={() => navigation.navigate('ShopNavigator')}
-      />
+
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}>
+        {MARKERS_DATA.map(marker => (
+          <Marker
+            key={marker.id}
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}
+          />
+        ))}
+      </MapView>
     </View>
   );
 };
@@ -19,8 +58,14 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   container: {
-    flex: 1,
-    padding: 16,
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
